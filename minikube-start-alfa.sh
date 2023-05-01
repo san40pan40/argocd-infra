9f253590a304768ca1a469b75cb7d61dbeff693f
Oleksandr Mozghovoi <mozghovoi.oleksandr@airslate.com>
2023-05-01 15:03:59 +0300

    add wait-for-redis

--- minikube-start-alfa.sh
+++ minikube-start-alfa.sh
@@ -1,2 +1,4 @@
-#/bin/bash
+#!/bin/bash
+minikube start --kubernetes-version=v1.22.10 -p alfa
+minikube profile alfa
 helm upgrade --create-namespace -i -n argocd argocd oci://registry-1.docker.io/bitnamicharts/argo-cd

da990527e62b1019f21e9479ba0710c347f69644
Oleksandr Mozghovoi <mozghovoi.oleksandr@airslate.com>
2023-05-02 11:32:26 +0300

    disable nginx 2

--- kubectl_port-forward_argocd.sh
+++ kubectl_port-forward_argocd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-kubectl port-forward --namespace argocd svc/argocd-argo-cd-server 8080:80 &
 export URL=http://127.0.0.1:8080/
 echo "Argo CD URL: http://127.0.0.1:8080/"
 
 echo "Username: \"admin\""
 echo "Password: $(kubectl -n argocd get secret argocd-secret -o jsonpath="{.data.clearPassword}" | base64 -d)"
+
+kubectl port-forward --namespace argocd svc/argocd-argo-cd-server 8080:80
